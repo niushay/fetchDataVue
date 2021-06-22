a78c3e8b90bc5842bce3b54ebacfac2e29a27902
Niusha <ext.n.yousefi@mci.ir>
2021-06-22 21:37:42 +0430

    fetch data from json

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,14 +1,27 @@
 <template>
-    <h1>Job Details Page</h1>
-    <p>The job id is {{ id }}</p>
+    <div v-if="job">
+        <h1>{{ job.title }}</h1>
+        <p>The job id is {{ id }}</p>
+        <p>{{ job.details }}</p>
+    </div>
+    <div v-else>
+        <p>Loading job details...</p>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            id: this.$route.params.id
+            id: this.$route.params.id,
+            job: null
         }
+    },
+    mounted() {
+        fetch("http://localhost:3000/jobs/" + this.id)
+        .then(res => res.json())
+        .then(data => this.job = data)
+        .catch(err => console.log(err.message))
     },
 
 }
--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,22 +1,29 @@
 <template>
-  <h1>jobs</h1>
-  <div v-for="job in jobs" :key="job.id">
+<div v-if="jobs.length">
+      <h1>jobs</h1>
+      <div v-for="job in jobs" :key="job.id">
       <router-link :to="{ name: 'JobDetails', params: { id: job.id }}">
           <h2>{{ job.title }}</h2>
       </router-link>
-  </div>
+      </div>
+    </div>
+<div v-else>
+    <p>Loading jobs...</p>
+</div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            jobs : [
-                { title: 'job title 1', id:1, details: 'lorem' },
-                { title: 'job title 2', id:2, details: 'lorem' },
-                { title: 'job title 3', id:3, details: 'lorem' }
-            ]
+            jobs : []
         }
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs')
+        .then(res => res.json())
+        .then(data => this.jobs = data)
+        .catch(err => console.log(err.message))
     },
 }
 </script>
